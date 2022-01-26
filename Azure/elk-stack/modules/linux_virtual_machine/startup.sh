305e4d863f201a52722f8cadace45161d2a602bd
ITsLoveCats <67352969+ITsLoveCats@users.noreply.github.com>
2022-01-26 23:28:43 +0800

    added nginx reverse proxy

--- Azure/elk-stack/modules/linux_virtual_machine/startup.sh
+++ Azure/elk-stack/modules/linux_virtual_machine/startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# elasticsearch stack
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 
 sudo apt install apt-transport-https -y
@@ -8,9 +9,20 @@ echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee 
 
 sudo apt update && sudo apt install elasticsearch -y
 
-sudo systemctl enable elasticsearch.service --now
+sudo systemctl enable --now elasticsearch.service
 
 sudo echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
 sudo echo "discovery.type: single-node" >> /etc/elasticsearch/elasticsearch.yml
 
 sudo systemctl restart elasticsearch.service
+
+
+# kibana Stack
+sudo apt-get update && sudo apt-get install kibana
+
+sudo systemctl enable --now kibana.service
+
+# nginx reverse proxy
+sudo apt install nginx -y
+
+sudo systemctl enable --now nginx
